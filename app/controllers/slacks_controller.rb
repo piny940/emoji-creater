5914aafe87a2029a590634b0a0d2851477dd5df5
piny940 <an.s.shoma@gmail.com>
2022-12-09 20:20:23 +0900

    slackにチャットを送信できるようにする

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -2,9 +2,21 @@ class SlacksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    p params
+    slack_params = params[:slack]
+    p slack_params
     return render json: {
-    }, status: 400 if params[:token] != ENV['SLACK_VERIFICATION_TOKEN']
+    }, status: 400 if slack_params[:token] != ENV['SLACK_VERIFICATION_TOKEN']
+
+    return render json: {
+    }, status: 200 if slack_params[:event][:bot_id].present?
+
+    text = slack_params[:event][:text]
+    client = Slack::Web::Client.new
+    client.chat_postMessage(
+      channel: slack_params[:event][:channel],
+      text: 'テスト',
+      as_user: true
+    )
 
     render json: {
     }, status: 200
